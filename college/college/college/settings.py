c18a5aa7b2ba93e00d2ef1d29a329348a7af2a52
Suraj <pjrps2003@yahoo.com>
2024-08-30 00:49:25 +0530

    reset the all

--- college/college/college/settings.py
+++ college/college/college/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4$^#x_d^(k0(^rny%9mm2_a4ek45#ql*ks+jao93fg(e_cfmd8'
+SECRET_KEY = 'django-insecure-u1z(353^+-l_umk-%7_l%5u^eeyy_+w5f&h+20#2yx+9bqywd0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

c6bf69e12350215c221f1faaaa8ce10b113ce0b0
Suraj <pjrps2003@yahoo.com>
2024-09-14 17:58:19 +0530

    shell - migration

--- hospital/home/models.py
+++ hospital/home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    #id=models.AutoField()
+    name= models.CharField(max_length=150)
+    Age=models.IntegerField(default=25)
+    Address=models.TextField()
+    #file=models.FileField()
---
+++ hospital/home/utils.py
@@ -0,0 +1,8 @@
+from .models import student
+import time
+def run_function():
+    print("Hello start :- ")
+    time.sleep(0.51)
+    print("Mid :- ")
+    time.sleep(0.51)
+    print("End :-  ")
